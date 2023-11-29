49757b73e0844f367ceae778c39bf461dfe463f8
mjsadbulkadir <mjsabdulkadir123@gmail.com>
2023-11-29 16:51:56 +0530

    first commit

---
+++ shop/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+urlpatterns = [ 
+   path("",views.index, name="ShopHome"),            
+   path("about/",views.about, name="AboutUs"),           
+   path("contact/",views.contact, name="ContactUs"),           
+   path("tracker/",views.tracker, name="Trackingtatus"),           
+   path("search/",views.search, name="Search"),           
+   path("products/<int:myid>",views.productview, name="Productview"),           
+   path("checkout/",views.checkout, name="Checkout"),           
+   path("handlerequest/", views.handlerequest, name="handleRequest"),           
+]
